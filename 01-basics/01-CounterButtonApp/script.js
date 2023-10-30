a902a6d306e54d06789d198613daa5700d7176de
a.gridin <a.gridin@qiwi.com>
2023-10-30 15:12:43 +0300

    01-CounterButtonApp

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,15 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
-
 // Создайте Vue приложение
+import {createApp,defineComponent} from "./vendor/vue.esm-browser";
+const App = defineComponent({
+  name:'App',
+  data() {
+    return {
+      counter: 0
+    };
+  },
+  template: '<button type="button" @click="counter++">{{ counter }}</button>'
+});
+
+const app = createApp(App);
+const vm = app.mount('#app');
+window.vm = vm;
