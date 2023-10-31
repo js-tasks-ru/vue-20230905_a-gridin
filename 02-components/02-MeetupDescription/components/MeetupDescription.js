87e01619c1aa196c851d3c029b2aa22a8fe00d31
a.gridin <a.gridin@qiwi.com>
2023-10-31 05:10:54 +0300

    Revert "Added task MeetupDescription"
    This reverts commit 74755e17a97dcec78397683760161e20ebdd40e6.

--- 02-components/02-MeetupDescription/components/MeetupDescription.js
+++
@@ -1,7 +0,0 @@
-import { defineComponent } from '../vendor/vue.esm-browser.js';
-
-export default defineComponent({
-  name: 'MeetupDescription',
-
-  template: `<p class="meetup-description">Описание должно быть здесь</p>`,
-});
