aa6097c26a36cbf689d387f96b8c6f98a31d40b7
a.gridin <a.gridin@qiwi.com>
2023-10-31 05:11:04 +0300

    Revert "Added task Компонент CounterButton"
    This reverts commit 23add7c95750ce1c0c24e2e8ec36f583ead50983.

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++
@@ -1,9 +0,0 @@
-import { defineComponent } from './vendor/vue.esm-browser.js';
-
-export default defineComponent({
-  name: 'CounterButton',
-
-  // Компонент должен иметь входной параметр и порождать событие
-
-  template: `<button type="button">1</button>`,
-});
