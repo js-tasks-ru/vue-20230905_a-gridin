67baee12af6cdae99466d2a26dbfd8ddc1cc1f8a
a.gridin <a.gridin@qiwi.com>
2023-11-09 12:41:44 +0300

    02-components

---
+++ 02-components/05-MeetupAgenda/components/MeetupAgendaItem.js
@@ -0,0 +1,40 @@
+import { defineComponent } from '../vendor/vue.esm-browser.js';
+import { agendaItemIcons, agendaItemDefaultTitles } from '../meetupService.js';
+
+export default defineComponent({
+  name: 'MeetupAgendaItem',
+  props: {
+    agendaItem: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    iconURL() {
+      return `/assets/icons/icon-${agendaItemIcons[this.agendaItem.type]}.svg`;
+    },
+    title() {
+      return this.agendaItem.title ? this.agendaItem.title : agendaItemDefaultTitles[this.agendaItem.type];
+    },
+    timeInterval() {
+      return `${this.agendaItem.startsAt} - ${this.agendaItem.endsAt}`;
+    },
+  },
+  template: `
+    <div class="agenda-item">
+      <div class="agenda-item__col">
+        <img :src="iconURL" class="icon" alt="key" />
+      </div>
+      <div class="agenda-item__col">{{ timeInterval }}</div>
+      <div class="agenda-item__col">
+        <h3 class="agenda-item__title">
+          {{ title }}</h3>
+        <p v-if="agendaItem.type === 'talk'" class="agenda-item__talk">
+          <span>{{ agendaItem.speaker }}</span>
+          <span class="agenda-item__dot"></span>
+          <span class="agenda-item__lang"> {{ agendaItem.language }}</span>
+        </p>
+        <p v-if="agendaItem.description">{{ agendaItem.description }}</p>
+      </div>
+    </div>`,
+});
